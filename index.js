c37710b858993126fda19b023836f019ef471133
maude sos <mualth@gmail.com>
2017-09-18 01:36:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ let updateObjectWithKeyAndValue = (object, key, value) => {
   object[key] = value
   return object
 }
+
+let destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
+  object.key = [value]
+}
